0867392ae8a03db39078b13e84fc00e69a9146f4
VealM <626859951@qq.com>
2020-08-02 11:05:40 +0800

    Site updated: 2020-08-02 11:05:39

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "My Soul",
+        artist: 'July',
+        url: 'http://sp.sycdn.kuwo.cn/03409f22baffa89f2caf9fd7d2a5ade2/5f262bcf/resource/n3/76/59/813565089.mp3',
+        cover: 'https://image.bugsm.co.kr/album/images/original/3239/323979.jpg',
+    }
+    ]
+});
